06cfaa85b6c61011e98d4047d0b1a0bb1d405e4b
BRBrewster <186741@student.asd20.org>
2018-12-16 17:42:58 -0700

    Maze 4 finished. Maze 5 created

--- Buuny Maze/Assets/Scripts/ColorChange.cs
+++ Buuny Maze/Assets/Scripts/ColorChange.cs
@@ -11,19 +11,8 @@ public class ColorChange : MonoBehaviour {
     SpriteRenderer renderReference;
     void OnCollisionEnter2D(Collision2D coll)
     {
-        //if (coll.other.tag == "Wall") { //uncomment to check tag of colliding object
-        //if (counter < colors.Count - 1)
-        //{
-            //counter++;
-        //}
-        //else
-        //{
-            //counter = 0;
-        //}
         GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
-    //} //uncomment to check tag of colliding object
-
 
     void Start()
     {
