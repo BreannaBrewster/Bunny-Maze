25d58f763f0de0045e8a207d968731a3d680fdab
BRBrewster <186741@student.asd20.org>
2018-12-20 15:12:05 -0700

    Maze 5: invisible maze finished

---
+++ Buuny Maze/Assets/Scripts/ColorChangeBlack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeBlack : MonoBehaviour {
+
+    // Use this for initialization
+    int counter = 0;
+    int lightning = 0;
+    SpriteRenderer renderReference;
+
+    void Start()
+    {
+        renderReference = GetComponent<SpriteRenderer>();
+        GetComponent<Renderer>().material.color = Color.clear;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Renderer>().material.color != Color.white)
+        {
+            counter++;
+        }
+        if (counter >= 200)
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            lightning++;
+            
+        }
+        if(lightning >=10)
+        {
+            GetComponent<Renderer>().material.color = Color.clear;
+            counter = 0;
+            lightning = 0;
+        }
+    }
+
+}
---
+++ Buuny Maze/Assets/Scripts/lightning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightning : MonoBehaviour {
+
+    int counter = 0;
+    int flash = 0;
+    SpriteRenderer renderReference;
+
+    void Start()
+    {
+        renderReference = GetComponent<SpriteRenderer>();
+        GetComponent<Renderer>().material.color = Color.clear;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter >= 199)
+        {
+            GetComponent<Renderer>().material.color = Color.clear;
+            flash++;
+
+        }
+        if (flash >= 10)
+        {
+            GetComponent<Renderer>().material.color = Color.clear;
+            counter = 0;
+            flash = 0;
+        }
+    }
+}
